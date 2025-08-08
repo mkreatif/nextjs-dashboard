6ff669d3939581beed545207b5f90dad31c1915b
Margono <marlina.kreatif@gmail.com>
2025-08-08 14:38:48 +0700

    core : fixing configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: false,
+  // basePath: '', // kalau perlu, isi jika kamu deploy di subfolder
+  images: {
+    domains: ["marlinastudio.my.id"], // jika kamu pakai <Image />
+  },
 };
 
 export default nextConfig;
